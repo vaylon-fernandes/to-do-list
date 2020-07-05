8a1375e52d19ee05cc12332bb7ef865c135c98cc
vaylon-fernandes <vaylonfernandes@gmail.com>
2020-07-05 18:45:21 +0530

    1. Added docstring in all functions

--- pack/to_do_list.py
+++ pack/to_do_list.py
@@ -26,67 +26,114 @@ cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS tasks (task text)")
 #Functions
 def update_listbox():
+	"""
+    Clears the listbox and 
+    then updates the listbox
+    """
 	clear_listbox()
 	for task in tasks:
 		lb_list_box.insert("end",task)
 	conn.commit()
 def clear_listbox():
+	'''Clears the listbox'''
 	lb_list_box.delete(0,"end")
 
 def add_task():
-	task = txt_input.get()
-	#append to the list if input is not empty
-	if task !="":
-		tasks.append(task)
-		cur.execute('INSERT INTO tasks values(?)',(task,))
-		update_listbox()
-	else:
-		messagebox.showwarning("Warning","You need to enter a task")
-	txt_input.delete(0,"end")
+    """Adds a task entered from the input field to the listbox.
+    Also appends the task to the list.
+    Shows a warning if the input box is empty"""
+     
+    task = txt_input.get() 
+    
+     #append to the list if input field is not empty
+    if task !="":
+        tasks.append(task)
+        cur.execute('INSERT INTO tasks values(?)',(task,))
+        conn.commit()
+        update_listbox()
+    else:
+        messagebox.showwarning("Warning","You need to enter a task")
+
+    #clear the input field
+    txt_input.delete(0,"end")
+
+def on_return(event):
+    '''Calls the add_task() function
+    when the "Enter" key is pressed
+    in the Entry widget'''
+    add_task()
 
 def del_all():
-#global as we are changing the list
-#the tasks list has to be updated globally
-	global tasks
-	confirm = messagebox.askyesno("Confirm: Delete all","Do you want to delete all?")
-	if confirm:	
-		tasks = []
-		cur.execute('DELETE FROM tasks')
-	update_listbox()
+    """
+    Deletes all listbox entries.
+    Shows warning if list is empty.
+    """
+    #global as we are changing the list
+    #the tasks list has to be updated globally
+    global tasks
+    
+    if tasks != []:
+        confirm = messagebox.askyesno("Confirm: Delete all","Do you want to delete all?")
+        if confirm:
+            tasks = []
+
+            cur.execute("DELETE FROM tasks")
+            conn.commit()
+            update_listbox()
+        
+    else:
+        messagebox.showwarning("Warning","The list is empty!!")
 
 def del_one():
-	task = lb_list_box.get("active")
-	confirm = messagebox.askyesno("Confirm: Delete ","Do you want to delete?")
-	if task in tasks and confirm:
-		tasks.remove(task)
-		cur.execute('DELETE FROM tasks WHERE task = (?)',(task,))
-		update_listbox()
-	else: 
-		messagebox.showwarning("Warning","There is no task to delete")
+    """
+    Deletes selcted task from the list box.
+    If no task is selected, the tasks are
+    deleted in a First In First Out order
+    """
+    
+    task = lb_list_box.get("active")
+    if tasks != []:
+        confirm = messagebox.askyesno("Confirm: Delete ","Do you want to delete?")
+        if task in tasks and confirm:
+            tasks.remove(task)
+            cur.execute('DELETE FROM tasks where task is (?)',(task,))
+            conn.commit()
+            update_listbox()
+    else:
+        messagebox.showwarning("Warning","The list is empty!!")
 
 
 def choose_random():
-	task = random.choice(tasks)
-	lbl_display["text"] = task
+    """
+    Chooses a task at random and displays it on the label
+    """
+    task = random.choice(tasks)
+    lbl_display["text"] = task
 
 def show_number_tasks():
-	tasks_number = len(tasks)
-	msg = f"Number of Tasks: {tasks_number}"
-	lbl_display["text"] = msg
-
-def on_return(event):
-	add_task()
+    '''Displays the number of tasks 
+    currently present in the listbox '''
+    tasks_number = len(tasks)
+    msg = f"Number of Tasks: {tasks_number}"
+    lbl_display["text"] = msg
 
 def ex():
-	confirm = messagebox.askyesno(title="Exit",message="Do you want to leave?")
-	if confirm:
-		exit()
-
+    '''Asks the user if they would like to 
+    leave the app. If true closes the app.'''
+    confirm = messagebox.askyesno("Exit","Do you want to exit")
+    if confirm:
+        exit()
+    
 def reload_data():
+    '''Retrieves tasks from the database 
+    if it is not empty and displays them 
+    in the listbox'''
     global tasks
     tasks = []
     for data in cur.execute('SELECT task from tasks'):
         tasks.append(data)
+    update_listbox()
+
 #setup
 lbl_title = tkinter.Label(root,text="To-Do List",bg="white")
 lbl_title.pack()
