ad40f8562f4b6ced2168b568029f48b1d963f064
vaylon-fernandes <vaylonfernandes@gmail.com>
2020-11-06 16:13:22 +0530

    Updated venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vaylon\Documents\github\to-do-list\venv
+set VIRTUAL_ENV=C:\Users\Shanti\Documents\GitHub\to-do-list\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
